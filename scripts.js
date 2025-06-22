48c0d96dd3791718460d313803c10c7a22968610
Manu Cabello <emmanuelrcabellop056@gmail.com>
2025-06-22 15:32:24 -0300

    Bloque del boton generar matriz

--- scripts.js
+++ scripts.js
@@ -10,4 +10,25 @@
 // 4. Mostrar mensajes al usuario en el <p id="resultado">
 // 5. Validar que haya inicio, fin y que no esté bloqueado el camino
 
-const input
+const inputFilas = document.getElementById('inputFilas');
+const inputColumnas = document.getElementById('inputColumnas');
+const botonGenerarMatriz = document.getElementById('botonGenerarMatriz');
+const gridContainer = document.getElementById('gridContainer');
+const reiniciarGrid = document.getElementById('reiniciarGrid');
+
+//crea la matriz inicial apartir de los inputs del usuario
+botonGenerarMatriz.addEventListener('click', () => {
+    const filas = parseInt(inputFilas.value);
+    const columnas = parseInt(inputColumnas.value);
+
+    if (isNaN(filas) || isNaN(columnas) || (filas && columnas)< 0 ) {
+        console.log("Ingrese nuevamente las filas y columnas")
+        return;
+
+    }
+
+    //Llamamos a la funcion de genera matriz 
+
+    generarGrid(filas, columnas);
+    
+});
